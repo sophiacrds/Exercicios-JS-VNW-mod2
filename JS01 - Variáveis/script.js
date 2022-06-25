73c799c246d446553fa139e0d646db1df704a9b0
Sophia Leão <sophialeaocardoso@hotmail.com>
2022-06-25 18:38:24 -0400

    Atividade 01 finalizada

---
+++ JS01 - Variáveis/script.js
@@ -0,0 +1,10 @@
+let nome = "macarronada";
+let num = 10;
+let ano = 2022 - num;
+
+console.log(ano + num);
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
